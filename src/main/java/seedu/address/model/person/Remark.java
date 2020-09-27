c4dc7da4abb11aea41af1512a8285ffb7b1a046b
dearvae <lovevae7@gmail.com>
2020-09-27 17:10:28 +0800

    update style

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructs a {@code Phone}.
+     *
+     * @param remark A valid Remark.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
